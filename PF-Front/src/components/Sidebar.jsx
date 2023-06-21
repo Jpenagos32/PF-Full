032a038fe220f50c2b91256067f6395d3994c83a
Hernando Rey <hrking31@gmail.com>
2023-06-21 11:48:28 -0500

    se crea componente sidebar y css provicional

--- PF-Front/src/components/Sidebar.jsx
+++ PF-Front/src/components/Sidebar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./views/SideBar.module.css";
 
-const sidebar = () => {
-    return <div>sidebar</div>;
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <ul className={styles["sidebar-menu"]}>
+        <li className={styles["sidebar-menu-item"]}>Start rating</li>
+        <li className={styles["sidebar-menu-item"]}>Acomodation</li>
+        <li className={styles["sidebar-menu-item"]}>Facilities</li>
+      </ul>
+    </div>
+  );
 };
 
-export default sidebar;
+export default Sidebar;
--- PF-Front/src/components/views/Home.jsx
+++ PF-Front/src/components/views/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import Sidebar from "../Sidebar";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-            <section>
-                <NavLink to="/">Landing</NavLink>;
-                <NavLink to="/pay">PayView</NavLink>;
-            </section>
-        </div>
-    );
+  return (
+    <div>
+      Home
+      <section>
+        <Sidebar />
+        <NavLink to="/">Landing</NavLink>;<NavLink to="/pay">PayView</NavLink>;
+      </section>
+    </div>
+  );
 };
 
 export default Home;
