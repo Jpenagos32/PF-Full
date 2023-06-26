16a82398078a3e5e87fe813a2a681baf38e1f8e3
ilcjo <ileanacanoic@gmail.com>
2023-06-25 20:54:48 -0500

    refactor(Filter) quitar el contenedor de cada checkbox

--- PF-Front/src/components/Filters/Filters.jsx
+++ PF-Front/src/components/Filters/Filters.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Box from '@mui/joy/Box';
 import Checkbox from '@mui/joy/Checkbox';
 import List from '@mui/joy/List';
 import ListItem from '@mui/joy/ListItem';
@@ -31,39 +30,40 @@ export default function Filters() {
                 }
             }>
 
-                <Typography id="sandwich-group" level="body2" fontWeight="lg" mb={1}>
+                <Typography id="type_room" level="body2" fontWeight="lg" mb={1}>
                     Acomodation Type
                 </Typography>
-                <Box role="group" aria-labelledby="sandwich-group">
-                    <List size="sm">
-                        <ListItem>
-                            <Checkbox label="Standar Room" defaultChecked />
-                        </ListItem>
-                        <ListItem>
-                            <Checkbox label="Familiar Room" />
-                        </ListItem>
-                        <ListItem>
-                            <Checkbox label="Suite" />
-                        </ListItem>
-                    </List>
-                </Box>
 
-                <Typography id="sandwich-group" level="body2" fontWeight="lg" mb={1}>
+
+                <List size="sm">
+                    <ListItem>
+                        <Checkbox label="Standar Room" defaultChecked />
+                    </ListItem>
+                    <ListItem>
+                        <Checkbox label="Familiar Room" />
+                    </ListItem>
+                    <ListItem>
+                        <Checkbox label="Suite" />
+                    </ListItem>
+                </List>
+
+
+                <Typography id="facilities" level="body2" fontWeight="lg" mb={1}>
                     Facilities
                 </Typography>
-                <Box role="group" aria-labelledby="sandwich-group">
-                    <List size="sm">
-                        <ListItem>
-                            <Checkbox label="HD-TV" defaultChecked />
-                        </ListItem>
-                        <ListItem>
-                            <Checkbox label="Room-service" />
-                        </ListItem>
-                        <ListItem>
-                            <Checkbox label="Parking" />
-                        </ListItem>
-                    </List>
-                </Box>
+
+                <List size="sm">
+                    <ListItem>
+                        <Checkbox label="HD-TV" defaultChecked />
+                    </ListItem>
+                    <ListItem>
+                        <Checkbox label="Room-service" />
+                    </ListItem>
+                    <ListItem>
+                        <Checkbox label="Parking" />
+                    </ListItem>
+                </List>
+
             </Card>
         </>
 
