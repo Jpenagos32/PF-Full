3c526fe94a6f33e0783b53677c306fe3c50c5915
jpenagos32 <jpenagos32@gmail.com>
2023-06-27 16:48:34 -0500

    feat(filterRooms.js): Agregar Filtros

--- PF-Back/src/controllers/RoomsControllers/filterRooms.js
+++ PF-Back/src/controllers/RoomsControllers/filterRooms.js
@@ -1,28 +1,37 @@
 const Room = require('../../models/Room');
 
 const filterRooms = async (req, res) => {
-    try {
-        const { price, room_type, facilities } = req.body;
-        const query = {}
-        console.log(price);
-        if (price) {
-            query.price = price
-        }
-        if (room_type) {
-            query.room_type = room_type
-        }
-        if (facilities) {
-            let facilitiesArray = [];
-            facilities.forEach((element) => {
-                facilitiesArray.push(element);
-            });
-            query.facilities = { $in: facilitiesArray };
-        }
-        const filteredRooms = await Room.find(query)
-        res.status(200).json(filteredRooms)
-    } catch (error) {
-        res.status(402).json({ error: error.message })
-    }
-}
+	try {
+		const { price, room_type, facilities, capacity } = req.body;
+		const query = {
+			available: true,
+		};
+
+		if (price) {
+			query.price = { $gte: price };
+		}
+
+		if (room_type) {
+			query.room_type = room_type;
+		}
+
+		if (facilities) {
+			let facilitiesArray = [];
+			facilities.forEach((element) => {
+				facilitiesArray.push(element);
+			});
+			query.facilities = { $in: facilitiesArray };
+		}
+
+		if (capacity) {
+			query.capacity = { $gte: capacity };
+		}
+
+		const filteredRooms = await Room.find(query);
+		res.status(200).json(filteredRooms);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
+};
 
 module.exports = filterRooms;
