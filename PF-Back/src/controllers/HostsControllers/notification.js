ff16f6e4e9a8f36851080edab8fa25f210f16bb7
Juan Pablo Delgado Nivia <juanPDN@github.com>
2023-07-05 11:48:44 -0500

    refactor(notification.js): se modifica nitificaciones para recibir template de tipo de notificacion

--- PF-Back/src/controllers/HostsControllers/notification.js
+++ PF-Back/src/controllers/HostsControllers/notification.js
@@ -1,21 +1,26 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-const { USER, PASS } = process.env;
+const { USER_H, PASS } = process.env;
+const ejs = require('ejs')
+const path = require('path')
 
 const notification = async (usrData) => {
 	const transporter = nodemailer.createTransport({
 		service: 'Gmail',
 		auth: {
-			user: USER,
+			user: USER_H,
 			pass: PASS,
 		},
 	});
-	const { to, subject, text } = usrData;
+	const { to, subject, text, template } = usrData;
+
+	const templatePath = path.join(__dirname, '.', 'TemplateNotifications', template);
 	const mailOptions = {
-		from: USER,
+		from: USER_H,
 		to,
 		subject,
 		text,
+		html: await ejs.renderFile(templatePath),
 	};
 	await transporter.sendMail(mailOptions, (error, info) => {
 		if (error) {
@@ -26,5 +31,13 @@ const notification = async (usrData) => {
 	});
 };
 
+// const userData = {
+// 	to: 'jpdelgado9641@gmail.com',
+// 	subject: 'Reservation has been created',
+// 	text: 'Thanks for your reservation.',
+// 	template: 'notification.ejs'
+// };
+
+notification(userData);
 
 module.exports = notification;
