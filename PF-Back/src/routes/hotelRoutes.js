34c15fd552550f89b13e26a230c8301aa219861c
jpenagos32 <jpenagos32@gmail.com>
2023-07-06 17:05:03 -0500

    feat(validators): Agregar validaciones de hotel
    Se agregan las validaciones para las rutas post y put de /hotel
    Ahora se deben ingresar URLS correctas

--- PF-Back/src/routes/hotelRoutes.js
+++ PF-Back/src/routes/hotelRoutes.js
@@ -4,9 +4,14 @@ const hotelRoutes = express.Router();
 const postHotel = require('../controllers/HotelControllers/postHotel');
 const getHotel = require('../controllers/HotelControllers/getHotel');
 const putHotel = require('../controllers/HotelControllers/putHotel');
+const {
+	validatePostHotel,
+	validatePutHotel,
+} = require('../libraries/validators/hotelValidator');
+const validationMessages = require('../libraries/validators/validationMessages');
 
-hotelRoutes.post('/', postHotel);
+hotelRoutes.post('/', validatePostHotel, validationMessages, postHotel);
 hotelRoutes.get('/', getHotel);
-hotelRoutes.put('/', putHotel);
+hotelRoutes.put('/', validatePutHotel, validationMessages, putHotel);
 
 module.exports = hotelRoutes;
