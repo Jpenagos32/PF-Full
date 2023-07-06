b866e1d1d951747e018985f0d63f54e409775637
jpenagos32 <jpenagos32@gmail.com>
2023-07-06 14:42:17 -0500

    feat(hostRoutes): Agregar express-validator

--- PF-Back/src/routes/hostRoutes.js
+++ PF-Back/src/routes/hostRoutes.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const hostRoutes = express.Router();
+const { validationResult } = require('express-validator');
 
+const { validatePostHost } = require('../libraries/validators/hostValidator');
 const deleteHost = require('../controllers/HostsControllers/deleteHost');
 const getHostById = require('../controllers/HostsControllers/getHostById');
 const putHost = require('../controllers/HostsControllers/putHost');
 const postHost = require('../controllers/HostsControllers/postHost');
 
 hostRoutes.get('/', getHostById);
-hostRoutes.post('/', postHost);
+hostRoutes.post('/', validatePostHost, (req, res) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const errorArray = errors.array();
+		const errorMessages = errorArray.map((error) => error.msg);
+		return res.status(400).json(errorMessages);
+	}
+	postHost(req, res);
+});
 hostRoutes.delete('/:id', deleteHost);
 hostRoutes.put('/', putHost);
 
