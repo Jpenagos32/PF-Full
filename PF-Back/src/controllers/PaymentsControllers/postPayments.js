9d451613a88e37b4f623f0aeacc3c06b143559e0
unknown <juancho50301@gmail.com>
2023-07-06 07:26:52 -0500

    refactor(postPayments): se agruegan cambios para mejorar funcionalidad

--- PF-Back/src/controllers/PaymentsControllers/postPayments.js
+++ PF-Back/src/controllers/PaymentsControllers/postPayments.js
@@ -5,28 +5,41 @@ const { mercadopago } = require('../../utils/mercadoPago');
 const postPayments = async (req, res) => {
 	try {
 		const { identification, total } = req.body;
+		console.log("identification-> ",identification)
 		if (!identification) throw new Error('Must be provider identification');
-		const roomPay = await NewPayments.findOne({ identification });
-		if (roomPay.status==="approved") {
-			return res
-				.status(200)
-				.json({ message: 'The payment has already been registered' });
+		let roomPay = await NewPayments.findOne({ identification });
+		console.log("registro de pago->",roomPay);
+		if(roomPay===null){
+			roomPay = new NewPayments({
+				identification
+			});
+			await roomPay.save();
 		}
-		const host = await Host.findOne({ identification });
-		if (!host) {
+		
+		if(roomPay.status === "approved"){
+			
+				return res
+					.status(200)
+					.json({ message: 'The payment has already been arpove' });
+			
+		}
+		if(roomPay.status==="in progress" || "rejected"){
+
+		 const host = await Host.findOne({ identification });
+		 if (!host) {
 			return res.status(404).json({ error: 'Unregistered user' });
-		}
-		const { room_details } = host;
-		console.log(room_details.room_type)
-		const preference = {
-			items: [
+		 }
+		 const { room_details } = host;
+		 console.log(room_details.room_type)
+		 const preference = {
+			 items: [
 				{
 					title: room_details.room_type,
 					unit_price: total,
 					quantity: 1,
 				},
-			],
-			back_urls: {
+			 ],
+			 back_urls: {
 				success:
 					'http://localhost:3001/payments/success',
 				failure:
@@ -35,18 +48,17 @@ const postPayments = async (req, res) => {
 				// 	'https://sunsetsandsdev.adaptable.app/payments/success',
 				// failure:
 				// 	'https://sunsetsandsdev.adaptable.app/payments/failure',
-			},
-		};
-		const response = await mercadopago.preferences.create(preference);
-		const { id, init_point } = response.body;
-		const newPayment = new NewPayments({
-			ref_mp: id,
-			identification
-		});
-		await newPayment.save();
-		res.status(200).json({ init_point });
+			 },
+		   };
+		 const response = await mercadopago.preferences.create(preference);
+		 const { id, init_point } = response.body;
+		 await NewPayments.findOneAndUpdate ({identification},{ref_mp: id});
+		 await roomPay.save();
+		 res.status(200).json({ init_point });
+	    }
+
 	} catch (error) {
-		res.status(500).json({ error: 'Error al registrar el pago' });
+		res.status(500).json({ error: error.message });
 	}
 };
 
