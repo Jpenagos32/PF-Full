544369edeac245d49508a77ea6dc03ed5fa9d40a
jpenagos32 <jpenagos32@gmail.com>
2023-07-07 00:39:57 -0500

    fix(notificationRoutes): Eliminar archivo y ruta
    se eliminan por que al momento no son funcionales ni están disponibles

--- PF-Back/src/routes/index.js
+++ PF-Back/src/routes/index.js
@@ -4,14 +4,12 @@ const roomRoutes = require('./roomRoutes');
 const hostRoutes = require('./hostRoutes');
 const hotelRoutes = require('./hotelRoutes');
 const usersRoutes = require('./usersRoutes');
-const notificationRoutes = require('./notificationRoutes');
 const paymentsRoutes = require('./paymentsRoutes');
 
 router.use('/hosts', hostRoutes);
 router.use('/rooms', roomRoutes);
 router.use('/hotel', hotelRoutes);
 router.use('/users', usersRoutes);
-router.use('/notification', notificationRoutes);
 router.use('/payments', paymentsRoutes);
 
 module.exports = router;
--- PF-Back/src/routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const notificationRoutes = express.Router();
-
-const notification = require('../controllers/HostsControllers/notification');
-
-notificationRoutes.get('/', notification);
-
-module.exports = notificationRoutes;
