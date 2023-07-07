32cd890177127af403fb2885c95cec5785e0e01e
jpenagos32 <jpenagos32@gmail.com>
2023-07-06 22:49:10 -0500

    feat(roomValidator): Agregar validaciones a las rutas de room

--- PF-Back/src/routes/roomRoutes.js
+++ PF-Back/src/routes/roomRoutes.js
@@ -6,10 +6,15 @@ const getRoomsDetail = require('../controllers/RoomsControllers/getRoomsDetail')
 const getRooms = require('../controllers/RoomsControllers/getRooms');
 const postRooms = require('../controllers/RoomsControllers/postRooms');
 const deleteRooms = require('../controllers/RoomsControllers/deleteRooms');
+const {
+	validateGetRoom,
+	validatePostRooms,
+} = require('../libraries/validators/roomValidator');
+const validationMessages = require('../libraries/validators/validationMessages');
 
-roomRoutes.get('/', getRooms);
+roomRoutes.get('/', validateGetRoom, validationMessages, getRooms);
 roomRoutes.get('/:room_type', getRoomsDetail);
-roomRoutes.post('/', postRooms);
+roomRoutes.post('/', validatePostRooms, validationMessages, postRooms);
 roomRoutes.put('/', putRooms);
 roomRoutes.delete('/', deleteRooms);
 
