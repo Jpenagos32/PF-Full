b000e6f6ea16ea52c4785fd37aed945e2545b91d
Diegoaph <Diegoaph2018@gmail.com>
2023-07-08 10:51:15 -0500

    feat,refactor(cardsContainer,Landing)paginado de cards en home, refactor de responsividad de gallery en landing

--- PF-Front/src/components/CardsContainer/CardsContainer.jsx
+++ PF-Front/src/components/CardsContainer/CardsContainer.jsx
@@ -1,65 +1,84 @@
 /**
  * JavascripFile: CardContainer.js
- * Objetivo:  El componente CardsContainer es responsable de renderizar una
+ * Objetivo: El componente CardsContainer es responsable de renderizar una
  * colección de tarjetas de habitaciones. Cada tarjeta representa una habitación con diferentes atributos como nombre, imagen, rating, price, room service, queen bed y hd tv.
  * Autor: Hernando Rey
  * Fecha de creación: 21 de junio 2023
  *
- *
  *   - CardsContainer: La función principal del componente que renderiza el
  * contenedor de tarjetas de habitaciones.
  *   - map(): Una función de Array utilizada para iterar sobre la lista de habitaciones
- *(cardsRooms) y generar las tarjetas correspondientes.
+ * (cardsRooms) y generar las tarjetas correspondientes.
  *   - CardsContainer: No tiene parámetros de entrada ni salida.
  */
-import React from "react";
+import React, { useState } from "react";
 import CardRoom from "../CardRoom/CardRoom";
 import CardRoomSkeleton from "../CardRommSkeleton/CardRommSkeleton";
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Pagination } from "@mui/material";
 import { useSelector } from "react-redux";
 
 export default function CardsContainer() {
-  const loading = useSelector((state) => state.loading.loading);
-  const cardsRooms = useSelector((state) => state.rooms.rooms.filteredRooms);
-  const filteredrooms = useSelector((state) => state.rooms.rooms.filtered);
-  let roomsToShow = [];
+    const loading = useSelector((state) => state.loading.loading);
+    const cardsRooms = useSelector((state) => state.rooms.rooms.filteredRooms);
+    const filteredrooms = useSelector((state) => state.rooms.rooms.filtered);
+    const [currentPage, setCurrentPage] = useState(1);
+    const cardsPerPage = 12;
 
-  if (typeof filteredrooms !== "undefined") {
-    roomsToShow = filteredrooms;
-  } else {
-    roomsToShow = cardsRooms;
-  }
-  if (filteredrooms == undefined) {
-  }
+    let roomsToShow = [];
 
-  return (
-    <Container>
-      <Grid
-        container
-        rowSpacing={{ xs: 2, sm: 6, md: 8 }}
-        columnSpacing={{ xs: 2, sm: 6, md: 8 }}
-      >
-        {loading
-          ? Array.from(new Array(9)).map((_, index) => (
-              <CardRoomSkeleton key={index} />
-            ))
-          : roomsToShow &&
-            roomsToShow.map((room) => {
-              return (
-                <Grid item xs={12} sm={6} md={4} key={room._id}>
-                  <CardRoom
-                    _id={room._id}
-                    name={room.name}
-                    image={room.image.bed}
-                    price={room.price}
-                    facilities={room.facilities}
-                    room_type={room.room_type}
-                    room_number={room.room_number}
-                  />
-                </Grid>
-              );
-            })}
-      </Grid>
-    </Container>
-  );
+    if (typeof filteredrooms !== "undefined") {
+        roomsToShow = filteredrooms;
+    } else {
+        roomsToShow = cardsRooms;
+    }
+
+    const indexOfLastCard = currentPage * cardsPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    const currentCards = roomsToShow.slice(indexOfFirstCard, indexOfLastCard);
+    const totalPages = Math.ceil(roomsToShow.length / cardsPerPage);
+
+    const handlePageChange = (event, value) => {
+        setCurrentPage(value);
+    };
+
+    return (
+        <Container>
+            <Grid
+                container
+                rowSpacing={{ xs: 2, sm: 6, md: 8 }}
+                columnSpacing={{ xs: 2, sm: 6, md: 8 }}>
+                {loading
+                    ? Array.from(new Array(9)).map((_, index) => (
+                          <CardRoomSkeleton key={index} />
+                      ))
+                    : currentCards.map((room) => {
+                          return (
+                              <Grid
+                                  item
+                                  xs={12}
+                                  sm={6}
+                                  md={4}
+                                  key={room._id}>
+                                  <CardRoom
+                                      _id={room._id}
+                                      name={room.name}
+                                      image={room.image.bed}
+                                      price={room.price}
+                                      facilities={room.facilities}
+                                      room_type={room.room_type}
+                                      room_number={room.room_number}
+                                  />
+                              </Grid>
+                          );
+                      })}
+            </Grid>
+            <Grid style={{ margin: "4rem" }}>
+                <Pagination
+                    count={totalPages}
+                    page={currentPage}
+                    onChange={handlePageChange}
+                />
+            </Grid>
+        </Container>
+    );
 }
