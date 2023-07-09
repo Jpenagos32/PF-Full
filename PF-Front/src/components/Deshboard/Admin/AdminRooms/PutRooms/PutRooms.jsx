a58da296d79965ba7da38ea6fc59b2b6bf332ce0
Diegoaph <Diegoaph2018@gmail.com>
2023-07-08 22:07:32 -0500

    feat(PutRooms)se muestran y eliminn efectivamente las habitaciones)

--- PF-Front/src/components/Deshboard/Admin/AdminRooms/PutRooms/PutRooms.jsx
+++ PF-Front/src/components/Deshboard/Admin/AdminRooms/PutRooms/PutRooms.jsx
@@ -13,6 +13,8 @@ import {
     DialogContent,
     DialogActions,
     TextField,
+    Snackbar,
+    DialogContentText,
 } from "@mui/material";
 import { Delete, Edit } from "@mui/icons-material";
 
@@ -22,6 +24,10 @@ const PutRooms = () => {
     const [editModalOpen, setEditModalOpen] = useState(false);
     const [editRoomName, setEditRoomName] = useState("");
     const [editRoomNumber, setEditRoomNumber] = useState("");
+    const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
+    const [deleteRoomNumber, setDeleteRoomNumber] = useState("");
+    const [deleteSuccess, setDeleteSuccess] = useState(false);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
 
     useEffect(() => {
         fetchRooms();
@@ -36,19 +42,37 @@ const PutRooms = () => {
         }
     };
 
-    const handleDelete = async (roomId) => {
+    const handleDelete = async (num) => {
+        setDeleteRoomNumber(num);
+        setDeleteConfirmationOpen(true);
+    };
+
+    const confirmDelete = async () => {
         try {
-            await axios.delete(`/rooms/${roomId}`);
+            await axios.delete(`/rooms/?room_number=${deleteRoomNumber}`);
+            console.log(
+                `room ${deleteRoomNumber} successfully deleted from database`
+            );
+            setDeleteSuccess(true);
+            setSnackbarOpen(true);
             fetchRooms();
         } catch (error) {
-            console.log("Error deleting room:", error);
+            console.log(`Error deleting room ${deleteRoomNumber}:`, error);
+            setDeleteSuccess(false);
+            setSnackbarOpen(true);
         }
+        setDeleteConfirmationOpen(false);
+    };
+
+    const cancelDelete = () => {
+        setDeleteRoomNumber("");
+        setDeleteConfirmationOpen(false);
     };
 
     const handleEdit = (room) => {
         setEditingRoom(room);
         setEditRoomName(room.name);
-        setEditRoomNumber(room.roomNumber);
+        setEditRoomNumber(room.room_number);
         setEditModalOpen(true);
     };
 
@@ -72,6 +96,10 @@ const PutRooms = () => {
         setEditModalOpen(false);
     };
 
+    const handleSnackbarClose = () => {
+        setSnackbarOpen(false);
+    };
+
     return (
         <Box>
             <List>
@@ -91,7 +119,7 @@ const PutRooms = () => {
                             <IconButton
                                 edge="end"
                                 aria-label="delete"
-                                onClick={() => handleDelete(room.id)}>
+                                onClick={() => handleDelete(room.room_number)}>
                                 <Delete />
                             </IconButton>
                         </ListItemSecondaryAction>
@@ -131,6 +159,42 @@ const PutRooms = () => {
                     </Button>
                 </DialogActions>
             </Dialog>
+
+            <Dialog
+                open={deleteConfirmationOpen}
+                onClose={cancelDelete}>
+                <DialogTitle>Confirm Delete</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to delete Room Number:{" "}
+                        {deleteRoomNumber}?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        onClick={cancelDelete}
+                        color="primary">
+                        Cancel
+                    </Button>
+                    <Button
+                        onClick={confirmDelete}
+                        color="primary"
+                        variant="contained">
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={3000}
+                onClose={handleSnackbarClose}
+                message={
+                    deleteSuccess
+                        ? "Room deleted successfully!"
+                        : "Error deleting room."
+                }
+            />
         </Box>
     );
 };
